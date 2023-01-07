26791d96d5cfa787e7d3091020765ab6e8c50b9f
jfhovinne <git@hovinne.com>
2023-01-07 15:19:48 +0100

    fix: Use auth json file

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ pip install -r requirements.txt
 
 ## Usage
 
-Currently, only limited `build` command available.
+Currently, only `build` command available.
 
 ```
 Usage: bpcli.py build [OPTIONS] IMAGE
@@ -19,13 +19,13 @@ Usage: bpcli.py build [OPTIONS] IMAGE
   Generate app image from source code using the provided image name.
 
 Options:
-  --builder TEXT        Builder image  [default:
-                        paketobuildpacks/builder:base]
-  --path TEXT           Path to app dir  [default: .]
-  --log-level TEXT      Log level  [default: info]
-  --docker-config TEXT  Path to docker config directory with registry
-                        credentials
-  --help                Show this message and exit.
+  --builder TEXT             Builder image  [default:
+                             paketobuildpacks/builder:base]
+  --path TEXT                Path to app dir  [default: .]
+  --log-level TEXT           Log level  [default: info]
+  --docker-config-json TEXT  Path to docker config json file with registry
+                             credentials
+  --help                     Show this message and exit.
 ```
 
 ## Examples
@@ -42,5 +42,5 @@ registry authentication and report containing the image tag and digest.
   --path=/path/to/your/php/app \
   --builder=paketobuildpacks/builder:full \
   --log-level=debug foo/bar \
-  --docker-config=$HOME/.docker > report.toml
+  --docker-config-json=$HOME/.docker/config.json > report.toml
 ```
--- bpcli/bpcli.py
+++ bpcli/bpcli.py
@@ -18,17 +18,19 @@ def cli():
 @click.option('--builder', default='paketobuildpacks/builder:base', help='Builder image')
 @click.option('--path', default=str(Path()), help='Path to app dir')
 @click.option('--log-level', default='info', help='Log level')
-@click.option('--docker-config', default='', help='Path to docker config directory with registry credentials')
+@click.option('--docker-config-json', default='', help='Path to docker config json file with registry credentials')
 @click.argument('image')
-async def build(image, builder, path, log_level, docker_config):
+async def build(image, builder, path, log_level, docker_config_json):
     """Generate app image from source code using the provided image name."""
     async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
         src = client.host().directory(Path(path).resolve())
         cmd = f'/cnb/lifecycle/creator -app=/src -log-level={log_level} {image}'
         ctr = client.container().from_(builder)
-        if docker_config:
-            config = client.host().directory(Path(docker_config).resolve())
-            ctr = ctr.with_mounted_directory('/home/cnb/.docker', config)
+        if docker_config_json:
+            config_dir = client.host().directory(Path(docker_config_json).resolve().parent)
+            config_json = config_dir.file(Path(docker_config_json).resolve().name)
+            ctr = ctr.with_exec(['mkdir', '-p', '/home/cnb/.docker'])
+            ctr = ctr.with_file('/home/cnb/.docker/config.json', config_json)
         bp = (
             ctr := ctr.with_mounted_directory('/src', src)
             .with_user('root')
