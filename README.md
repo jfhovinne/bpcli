5b226370207e7a2d74567d0b413d840643111751
jfhovinne <git@hovinne.com>
2023-01-07 13:12:28 +0100

    feat: Add debug log level, report and registry auth

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # bpcli
 
-Inspired by [dagger-builpack](https://github.com/RealHarshThakur/dagger-builpack),
-but in Python.
+Uses [Dagger](https://dagger.io/) Python SDK to build an app with
+[buildpacks](https://buildpacks.io/) and pushes the built image to a registry.
 
 ## Install
 
@@ -14,23 +14,33 @@ pip install -r requirements.txt
 Currently, only limited `build` command available.
 
 ```
-./bpcli/bpcli.py build --help
 Usage: bpcli.py build [OPTIONS] IMAGE
 
   Generate app image from source code using the provided image name.
 
 Options:
-  --builder TEXT  Builder image  [default: paketobuildpacks/builder:base]
-  --path TEXT     Path to app dir  [default: .]
-  --help          Show this message and exit.
+  --builder TEXT        Builder image  [default:
+                        paketobuildpacks/builder:base]
+  --path TEXT           Path to app dir  [default: .]
+  --log-level TEXT      Log level  [default: info]
+  --docker-config TEXT  Path to docker config directory with registry
+                        credentials
+  --help                Show this message and exit.
 ```
 
 ## Examples
 
 ```
-./bpcli/bpcli.py build --path=/path/to/your/go/app ttl.sh/yourgoappimage:1h
+./bpcli/bpcli.py build --path=/path/to/your/go/app <your image name>
 ```
 
+Build a PHP application using the paketo full builder, with debug logs,
+registry authentication and report containing the image tag and digest.
+
 ```
-./bpcli/bpcli.py build --path=/path/to/your/php/app --builder=paketobuildpacks/builder:full ttl.sh/yourphpimage:2h
+./bpcli/bpcli.py build \
+  --path=/path/to/your/php/app \
+  --builder=paketobuildpacks/builder:full \
+  --log-level=debug foo/bar \
+  --docker-config=$HOME/.docker > report.toml
 ```
--- bpcli/bpcli.py
+++ bpcli/bpcli.py
@@ -15,24 +15,32 @@ def cli():
 
 
 @cli.command(context_settings={'show_default': True})
-@click.option('--builder', default="paketobuildpacks/builder:base", help='Builder image')
+@click.option('--builder', default='paketobuildpacks/builder:base', help='Builder image')
 @click.option('--path', default=str(Path()), help='Path to app dir')
+@click.option('--log-level', default='info', help='Log level')
+@click.option('--docker-config', default='', help='Path to docker config directory with registry credentials')
 @click.argument('image')
-async def build(image, builder, path):
+async def build(image, builder, path, log_level, docker_config):
     """Generate app image from source code using the provided image name."""
     async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
         src = client.host().directory(Path(path).resolve())
-        builder = (
-            client.container().from_(builder)
-            .with_mounted_directory('/src', src)
+        cmd = f'/cnb/lifecycle/creator -app=/src -log-level={log_level} {image}'
+        ctr = client.container().from_(builder)
+        if docker_config:
+            config = client.host().directory(Path(docker_config).resolve())
+            ctr = ctr.with_mounted_directory('/home/cnb/.docker', config)
+        bp = (
+            ctr := ctr.with_mounted_directory('/src', src)
             .with_user('root')
-            .with_exec(['chown', 'cnb:cnb', '/src'])
+            .with_exec(['chown', '-R', 'cnb:cnb', '/home/cnb'])
+            .with_exec(['chown', '-R', 'cnb:cnb', '/src'])
             .with_user('cnb')
-            .with_exec(['bash', '-c', f'/cnb/lifecycle/creator -app=/src -cache-dir=/home/cnb {image}'])
+            .with_exec(['bash', '-c', cmd])
         )
-        out = await builder.stdout()
+        report = ctr.file('/layers/report.toml')
+        stdout = await report.contents()
 
-    print(out)
+    print(stdout)
 
 
 if __name__ == '__main__':
