cc79d77a7f7aca68ec66e428e8e97d7d8d3d0c6a
jfhovinne <git@hovinne.com>
2023-01-25 17:00:33 +0100

    chore: Release 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='bpcli',
-    version='0.2.0',
+    version='0.2.1',
     author='jfhovinne',
     author_email='bpcli@hovinne.com',
     license='MIT',
